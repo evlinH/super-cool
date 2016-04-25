c90733f40d7675d0af2163f8f4b90dcb65418980
Humanayz <humanayz@Humanayzs-MacBook-Pro.local>
2016-04-25 21:04:26 +0300

    First commit/ added files

--- SuperCool/ViewController.swift
+++ SuperCool/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var superCoolLogo: UIImageView!
+    @IBOutlet weak var superCoolBG: UIImageView!
+    @IBOutlet weak var unCoolButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +23,10 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    @IBAction func MakeMeCool(sender: AnyObject) {
+        superCoolLogo.hidden = false
+        superCoolBG.hidden = false
+        unCoolButton.hidden = true
+    }
 }
 
